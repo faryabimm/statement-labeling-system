c1054a287fbef3e6fc59a76e1298f9dd5e4cfda0
faryabimohammadmahdi@live.com <faryabimohammadmahdi@gmail.com>
2019-10-08 01:00:27 +0330

    refactor settings module

--- crowd_labeling/settings.py
+++ crowd_labeling/settings.py
@@ -11,22 +11,15 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from crowd_labeling.production_secret import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!*0wifj@iyrnmk-j@4k)@x#ox@*8x5z*j*9@3nm5nvt@^moqv='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -73,7 +66,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'crowd_labeling.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -83,7 +75,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -103,7 +94,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -117,7 +107,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
@@ -127,7 +116,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = '/label'
 LOGOUT_REDIRECT_URL = '/label'
 
+# settings for viewing labels in admin panel
 ADMIN_STATEMENT_PREVIEW_CHARACTER_LIMIT = 100
 ADMIN_STATEMENT_RESPONSE_TIME_PRECISION = 2
 
+# custom logical settings
 MAX_LABELS_PER_STATEMENT = 3
+
