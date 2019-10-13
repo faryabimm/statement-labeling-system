2821d55c9db14f02505a8b0a14aa7d8fcc0956c0
Mohammadmahdi Faryabi <faryabimohammadmahdi@gmail.com>
2019-10-14 02:20:51 +0330

    update import script to support plain text files

--- crowd_labeling/management/commands/import_statements.py
+++ crowd_labeling/management/commands/import_statements.py
@@ -1,4 +1,5 @@
 import json
+from json.decoder import JSONDecodeError
 
 from django.core.management.base import BaseCommand
 
@@ -15,10 +16,16 @@ class Command(BaseCommand):
         file_path = options['file_path'][0]
 
         data = []
-        with open(file_path) as f:
-            for line in f:
-                for item in json.loads(line)['items']:
-                    data.append(item['content'])
+        try:
+            with open(file_path) as f:
+                for line in f:
+                    for item in json.loads(line)['items']:
+                        data.append(item['content'])
+        except JSONDecodeError:
+            with open(file_path) as f:
+                for line in f:
+                    data.append(line.strip())
+
         self.stdout.write('delete all previous statements')
         Statement.objects.all().delete()
         self.stdout.write('add all new statements')
@@ -26,5 +33,5 @@ class Command(BaseCommand):
         for i, record in enumerate(data):
             statement = Statement(text=record)
             statement.save()
-            self.stdout.write('\rsaved {i} out of {n}'.format(i=i+1, n=n), ending='')
+            self.stdout.write('\rsaved {i} out of {n}'.format(i=i + 1, n=n), ending='')
         self.stdout.write('\ndone')
