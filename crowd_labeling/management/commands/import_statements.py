40c62360e54b2e6dd65b238de75c3998fa395a46
Mohammadmahdi Faryabi <faryabimohammadmahdi@gmail.com>
2019-10-14 14:37:49 +0330

    refine import command, drop support for jl format

--- crowd_labeling/management/commands/import_statements.py
+++ crowd_labeling/management/commands/import_statements.py
@@ -1,13 +1,10 @@
-import json
-from json.decoder import JSONDecodeError
-
 from django.core.management.base import BaseCommand
 
 from crowd_labeling.models import Statement
 
 
 class Command(BaseCommand):
-    help = "Imports Statements from a .JL file"
+    help = "Imports statements from a .TXT file"
 
     def add_arguments(self, parser):
         parser.add_argument('file_path', nargs=1, type=str)
@@ -16,15 +13,9 @@ class Command(BaseCommand):
         file_path = options['file_path'][0]
 
         data = []
-        try:
-            with open(file_path) as f:
-                for line in f:
-                    for item in json.loads(line)['items']:
-                        data.append(item['content'])
-        except JSONDecodeError:
-            with open(file_path) as f:
-                for line in f:
-                    data.append(line.strip())
+        with open(file_path) as f:
+            for line in f:
+                data.append(line.strip())
 
         self.stdout.write('delete all previous statements')
         Statement.objects.all().delete()
@@ -33,5 +24,5 @@ class Command(BaseCommand):
         for i, record in enumerate(data):
             statement = Statement(text=record)
             statement.save()
-            self.stdout.write('\rsaved {i} out of {n}'.format(i=i + 1, n=n), ending='')
+            self.stdout.write('\r{p}% | saved {i}/{n}'.format(p=(i + 1) * 100 / n, i=i + 1, n=n), ending='')
         self.stdout.write('\ndone')
