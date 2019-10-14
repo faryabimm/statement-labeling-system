0ee735bcb066e2d84ff5881858fb5f6046adb600
Mohammadmahdi Faryabi <faryabimohammadmahdi@gmail.com>
2019-10-14 12:10:56 +0330

    update production settings add password

--- crowd_labeling/settings_production.py
+++ crowd_labeling/settings_production.py
@@ -6,6 +6,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': 5432,
     }
